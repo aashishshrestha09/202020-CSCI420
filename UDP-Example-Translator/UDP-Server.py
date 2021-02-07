4456a76d213cb2f330973183f0e7b83f3459f270
Paul Talaga <paul@fuzzpault.com>
2021-02-07 16:10:39 -0500

    UDP client/server example

---
+++ UDP-Example-Translator/UDP-Server.py
@@ -0,0 +1,28 @@
+"""
+UDP-SErver.py
+Paul Talaga
+Feb 7, 2020
+Desc: UDP Client/Server Demo
+
+"""
+import socket
+
+serverIP = '0.0.0.0' # Listen on all adapters
+serverPort = 1200
+
+print("Server Started.  Listening...")
+
+if __name__ == "__main__":
+    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
+    server.bind(  (serverIP,serverPort) )
+    while True:
+        (newMessage, clientAddress) = server.recvfrom(2048)
+        print("Got: {}".format(newMessage))
+        
+        newMessage = str(newMessage).upper()
+        
+        server.sendto(newMessage.encode('utf-8'), clientAddress)
+    
+    server.close()
+    
